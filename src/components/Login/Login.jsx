ca4976c7670693bad7f4cef54b158579a72ca691
Irina Berestova <berestovaecdep@mail.ru>
2022-12-01 03:05:38 +0700

    сделать резиновую для логина и регистрации

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,48 +3,50 @@ import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <main className="start-page">
-      <p className="start-page__title">Рады видеть!</p>
-      <form className="start-page__form">
-        <h3 className="input__title">E-mail</h3>
-        <input
-          placeholder="E-mail"
-          name="emailInput"
-          type="email"
-          className="start-page__input"
-          //   value={password}
-          required
-        />
-        <span
-          id="name-input-error"
-          className="form__error form__input_type_error form__error_visible"
-        />
-        <h3 className="input__title">Пароль</h3>
-        <input
-          placeholder="Пароль"
-          name="passwordInput"
-          type="password"
-          className="start-page__input"
-          //   value={password}
-          required
-        />
-        <span
-          id="name-input-error"
-          className="form__error form__input_type_error form__error_visible"
-        />
-        <button
-          type="submit"
-          className="start-page__button start-page__button_login"
-        >
-          Войти
-        </button>
-      </form>
-      <div className="start-page__signin start-page__signin_login">
-        <p className="start-page__question">Ещё не зарегистрированы?</p>
-        <Link to="/sign-up" className="start-page__register-link">
-          Регистрация
-        </Link>
-      </div>
+    <main className="main">
+      <section className="start-page">
+        <p className="start-page__title">Рады видеть!</p>
+        <form className="start-page__form">
+          <h3 className="input__title">E-mail</h3>
+          <input
+            placeholder="E-mail"
+            name="emailInput"
+            type="email"
+            className="start-page__input"
+            //   value={password}
+            required
+          />
+          <span
+            id="name-input-error"
+            className="form__error form__input_type_error form__error_visible"
+          />
+          <h3 className="input__title">Пароль</h3>
+          <input
+            placeholder="Пароль"
+            name="passwordInput"
+            type="password"
+            className="start-page__input"
+            //   value={password}
+            required
+          />
+          <span
+            id="name-input-error"
+            className="form__error form__input_type_error form__error_visible"
+          />
+          <button
+            type="submit"
+            className="start-page__button start-page__button_login"
+          >
+            Войти
+          </button>
+        </form>
+        <div className="start-page__signin start-page__signin_login">
+          <p className="start-page__question">Ещё не зарегистрированы?</p>
+          <Link to="/sign-up" className="start-page__register-link">
+            Регистрация
+          </Link>
+        </div>
+      </section>
     </main>
   );
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,58 +3,60 @@ import { Link } from "react-router-dom";
 
 function Register() {
   return (
-    <main className="start-page">
-      <p className="start-page__title">Добро пожаловать!</p>
-      <form className="start-page__form">
-        <h3 className="input__title">Имя</h3>
-        <input
-          placeholder="Имя"
-          name="nameInput"
-          type="text"
-          className="start-page__input"
-          //   value={email}
-          required
-        />
-        <span
-          id="name-input-error"
-          className="form__error form__input_type_error form__error_visible"
-        />
-        <h3 className="input__title">E-mail</h3>
-        <input
-          placeholder="E-mail"
-          name="emailInput"
-          type="email"
-          className="start-page__input"
-          //   value={password}
-          required
-        />
-        <span
-          id="name-input-error"
-          className="form__error form__input_type_error form__error_visible"
-        />
-        <h3 className="input__title">Пароль</h3>
-        <input
-          placeholder="Пароль"
-          name="passwordInput"
-          type="password"
-          className="start-page__input"
-          //   value={password}
-          required
-        />
-         <span
-          id="name-input-error"
-          className="form__error form__input_type_error form__error_visible"
-        />
-        <button type="submit" className="start-page__button">
-          Зарегистрироваться
-        </button>
-      </form>
-      <div className="start-page__signin">
-        <p className="start-page__question">Уже зарегистрированы?</p>
-        <Link to="/sign-in" className="start-page__register-link">
-          Войти
-        </Link>
-      </div>
+    <main className="main">
+      <section className="start-page">
+        <p className="start-page__title">Добро пожаловать!</p>
+        <form className="start-page__form">
+          <h3 className="input__title">Имя</h3>
+          <input
+            placeholder="Имя"
+            name="nameInput"
+            type="text"
+            className="start-page__input"
+            //   value={email}
+            required
+          />
+          <span
+            id="name-input-error"
+            className="form__error form__input_type_error form__error_visible"
+          />
+          <h3 className="input__title">E-mail</h3>
+          <input
+            placeholder="E-mail"
+            name="emailInput"
+            type="email"
+            className="start-page__input"
+            //   value={password}
+            required
+          />
+          <span
+            id="name-input-error"
+            className="form__error form__input_type_error form__error_visible"
+          />
+          <h3 className="input__title">Пароль</h3>
+          <input
+            placeholder="Пароль"
+            name="passwordInput"
+            type="password"
+            className="start-page__input"
+            //   value={password}
+            required
+          />
+          <span
+            id="name-input-error"
+            className="form__error form__input_type_error form__error_visible"
+          />
+          <button type="submit" className="start-page__button">
+            Зарегистрироваться
+          </button>
+        </form>
+        <div className="start-page__signin">
+          <p className="start-page__question">Уже зарегистрированы?</p>
+          <Link to="/sign-in" className="start-page__register-link">
+            Войти
+          </Link>
+        </div>
+      </section>
     </main>
   );
 }
